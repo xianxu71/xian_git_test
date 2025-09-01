41f5a61f931b46b98e3f21f71b8ea967375fc03a
xian71 <samson12138@gmail.com>
2025-08-31 22:13:18 -0700

    version1

---
+++ gittest1.py
@@ -0,0 +1,4 @@
+print('this is a test file')
+print('hello world')
+print('BerkeleyGW is a user-friendly software package')
+print('goodbye world')
---
+++ gittest2.py
@@ -0,0 +1,4 @@
+print('this is also a test file')
+print('I am going to say something')
+print('something')
+print('Goodbye')
